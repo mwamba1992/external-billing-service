4d3072223c5a7f5025c2d83caf5a776e3c832bce
mwamba1992 <joelgaitan1992@gmail.com>
2019-07-17 17:58:10 +0530

    initial

---
+++ src/main/java/com/examples/microserv/extbilling/ExternalBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examples.microserv.extbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalBillingServiceApplication.class, args);
+	}
+
+}
